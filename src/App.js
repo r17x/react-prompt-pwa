7ce88b88c257ae05e993aab483ad5f905a73401f
ri7nz <ri7nz.labs@gmail.com>
2019-06-25 22:30:55 +0700

    added: snackbar.css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App ">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Membuat Prompt PWA #4
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://medium.com/wwwid"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Baca Lebih Lanjut Di WWWID Yah
           </a>
         </header>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,86 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render, unmountComponentAtNode} from 'react-dom';
 import './index.css';
+import './snackbar.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const pwaPrompt = {
+    hasPrompt : () => window.localStorage.getItem('hasPrompt'),
+    setPrompt : yesOrNo => {
+        window.localStorage.setItem('hasPrompt', yesOrNo) 
+        window.location.reload(true) 
+    },
+    onUpdate: (reg) => {
+        const onUpdateElement = document.createElement('div')
+        render(
+            <>
+            <PromptPWA visible={true} updated={true} handleClick={e => { unmountComponentAtNode(onUpdateElement) }}/>
+            </>, onUpdateElement)
+        document.body.appendChild(onUpdateElement)
+    }
+}
+/**
+ * Functional Component
+ * ====================
+ * @param {object} props
+ * @return {Element}
+ */
+const PromptPWA = (props) => {
+    /**
+     * @name handleClick
+     * @param {object} e Event
+     * @return {void}
+     * @description Menyimpan nilai ke WebStorage | localStorage
+     */
+    const handleClick = e => {
+        props.setPrompt(e.currentTarget.value)
+        props.handleClick && props.handleClick()
+        // reg.update 
+        props.reload && props.reload()
+    }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+    return (
+        <div 
+          id="snackbar" 
+          className={props.updated || props.visible ? 'show' : undefined}>
+        <h3> 
+        {props.updated 
+            ? '[!] Halaman perlu diload ulang, Om 😋' 
+            : 'Mau Nyobain PWA ngk 😃?  '
+        }
+            <button onClick={handleClick} value={true}>
+            { props.updated 
+                ? 'RELOAD'
+                : 'IYA'
+            }
+            </button>
+            {' '}
+            <button onClick={handleClick} value={false}>
+            Tidak
+            </button>
+        </h3>
+        </div>
+    )
+}
+(() => {
+    pwaPrompt.hasPrompt() === 'true' && 
+        serviceWorker.register({onUpdate: pwaPrompt.onUpdate})
+    pwaPrompt.hasPrompt() === 'false' && 
+        serviceWorker.unregister()
+    const reactElement = document.createElement('div')
+    render(
+        <>
+            <PromptPWA 
+                updated={pwaPrompt.hasPrompt() === 'updated'}
+                setPrompt={pwaPrompt.setPrompt}
+                visible={!pwaPrompt.hasPrompt()}
+                reload={pwaPrompt.pwaUpdate}/>    
+            <App/>
+        </>, 
+        reactElement)
+    document.body.appendChild(reactElement)
+})()
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
